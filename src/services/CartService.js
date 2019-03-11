e2292e695312446610a68c5472fc871034ad29b9
rikgj <rikard.gjelsvik@lyse.net>
2019-03-11 12:54:32 +0100

    lagde services om til egen mappe

---
+++ src/services/CartService.js
@@ -0,0 +1,31 @@
+import { connection } from './mysql_connection';
+
+class CartService{
+  /*tableHome => hvor produktet hører hjemme, SYKKEL, UTSTYR, ...
+  cart[0] = [tableHome, colum1, colum2, colum3, colum4, colum5, ...];
+  switch (tableHome) {
+    case "tableName":
+      correctRefister(cart[0][1],cart[0][2],cart[0][3],cart[0][4],...);
+      break;
+    default:
+
+  }
+  */
+  constructor(){
+
+  }
+
+  removeCart(){
+
+  }
+
+  alterCart(){
+
+  }
+
+  addItem(item){
+
+  }
+}
+
+export let cartService = new CartService();
