f1f82c8840125e6a6ffd2fa1a33c24ce306e50ef
AGilberg <Adrian.B.Gilberg@Gmail.com>
2019-03-19 10:28:00 +0100

    Lagt til søkefunksjonalitet for kundedata. Velger per nå en søkekategori (tenker det blir ryddigst), men det går an å endre til generelt søk også.

--- src/components/kundesok.js
+++ src/components/kundesok.js
@@ -3,6 +3,7 @@ import { Component } from 'react-simplified';
 import { kundeService } from '../services/KundeService.js';
 import createHashHistory from 'history/createHashHistory';
 const history = createHashHistory();
+import Card from 'react-bootstrap/Card'
 
 class Kundesøk extends Component {
   kunder = [];
@@ -26,15 +27,19 @@ class Kundesøk extends Component {
 
         <div className="col-md-4">
           <br />
-          <input type="text" name="sok" id="sok" placeholder="Søk i kundedatabasen" />
-          <button id="kundesok" name="kundesok" className="btn btn-light" onClick={this.sok}>
-            Søk
-          </button>
+          <input type="text" name="sok" id="sok" placeholder="Søk i kundedatabasen" onChange={this.sok} />
+          <select id="kategori">
+            <option value="0">Navn</option>
+            <option value="1">Epost</option>
+            <option value="2">Telefon</option>
+            <option value="3">Adresse</option>
+            <option value="4">Fødselsdato</option>
+          </select>
         </div>
         <div id="kunderesultat">
           <div>
             {this.kunder.map(kunde => (
-              <div key={kunde.person_id}>
+              <Card key={kunde.person_id}>
                 <ul>
                   <li>Navn: {kunde.fornavn + ' ' + kunde.etternavn}</li>
                   <li>Mail: {kunde.mail}</li>
@@ -44,7 +49,7 @@ class Kundesøk extends Component {
                   {/* FIXME:TRENGER FORMATERING */}
                   <li>Kommentar: {kunde.kommentar}</li>
                 </ul>
-              </div>
+              </Card>
             ))}
           </div>
         </div>
@@ -62,9 +67,26 @@ class Kundesøk extends Component {
       this.kunder = kunder;
     });
   }
-  // sok() {
-  //// FIXME: rik fix
-  // }
+
+  sok() {
+    var input, filter, type, div, ul, i, li, txt;
+    input = document.getElementById("sok");
+    filter = input.value.toUpperCase();
+    type = document.getElementById("kategori").value;
+    div = document.getElementById("kunderesultat").children[0];
+    ul = div.getElementsByTagName("ul");
+    for (i = 0; i < this.kunder.length; i++) {
+      li = ul[i].getElementsByTagName("li");
+      txt = li[type].textContent || li[type].innerText;
+      console.log(ul, txt);
+      if (txt.toUpperCase().indexOf(filter) > -1) {
+        ul[i].style.display = "";
+      } else {
+        ul[i].style.display = "none";
+      }
+    }
+  }
+
   tilbake() {
     history.push('/kunde');
   }
