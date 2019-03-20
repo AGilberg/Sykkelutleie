53d5f24688ad833cde91bd3aa50920eaeae0e462
rikgj <rikard.gjelsvik@lyse.net>
2019-03-21 00:17:57 +0100

    fikset datoformatering

--- src/components/bestillingDetails.js
+++ src/components/bestillingDetails.js
@@ -98,7 +98,13 @@ class BestillingDetails extends Component {
 
   formateDate(date){
     let day = date.getDate();
+    if(day < 10){
+      day = "0" + day.toString();
+    }
     let month = date.getMonth() + 1;
+    if(month < 10){
+      month = "0" + month.toString();
+    }
     let year = date.getFullYear();
 
     return day + "/" + month + "/" + year;
