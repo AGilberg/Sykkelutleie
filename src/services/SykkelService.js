d84b0f8c06ab134a89b624826a2d12549a3120e8
rikgj <rikard.gjelsvik@lyse.net>
2019-03-22 03:20:49 +0100

    Sortering av sykler og ekstrautstyr

--- src/services/SykkelService.js
+++ src/services/SykkelService.js
@@ -1,9 +1,9 @@
 import { connection } from '../mysql_connection';
 
 class SykkelService{
-  getSykler(success){
+  getSykkeltyper(success){
     connection.query(
-      'select * from SYKKELTYPE',
+      'SELECT s.*, kl.klassenavn FROM SYKKELTYPE s, KLASSE kl  WHERE s.klasse_id = kl.klasse_id',
         (error, results) => {
         if(error) return console.error(error);
         success(results);
@@ -31,33 +31,15 @@ class SykkelService{
         });
     }
   }
-  sortSykkelSok(metode, arr){//retuner en sortert array
-    let index;
-    let nyArr = [];
-    switch (metode) {
-      case "alfAZ":
 
-        nyArr = arr.sort(sorteringfunk);
-        break;
-      case "alfZA":
-        break;
-      case "prisLH":
-        break;
-      case "prisHL":
-        break;
-      default:// FIXME: FJERN FØR FERDIG?
-        console.log("ingen metode valgt");
-    }
-      return sykkelArr;
+  getSykkelklasser(success){
+    connection.query(
+      'select * from KLASSE',
+        (error, results) => {
+        if(error) return console.error(error);
 
-    function sorteringfunk(a, b) {//brukes for å sortere array med array
-     if (a[index] < b[index]){
-       return -1;
-     }else if (a[index] > b[index]){
-       return 1;
-     }
-     return 0;
-   }
+        success(results);
+      });
   }
 
   getSykkelSorteringer(){
@@ -69,14 +51,59 @@ class SykkelService{
     return options;
   }
 
-  getSykkelklasser(success){
-    connection.query(
-      'select * from KLASSE',
-        (error, results) => {
-        if(error) return console.error(error);
+  sortSykkelsok(metode, arr, success){//retuner en sortert array
+    let index, r1, r2;
+    let mulighet = this.getSykkelSorteringer();
+    let nyArr = [];
+    switch (metode) {
+      case mulighet[0][0]://alfabetisk A-Z
+        index = "typenavn";
+        r1 = -1;
+        r2 = 1;
+        nyArr = arr.sort(sorteringfunk);
+        break;
+      case mulighet[1][0]://alfabetisk Z-A
+        index = "typenavn";
+        r1 = 1;
+        r2 = -1;
+        break;
+      case mulighet[2][0]://pris lav-høy
+        index = "pris";
+        r1 = -1;
+        r2 = 1;
+        break;
+      case mulighet[3][0]://pris høy-lav
+        index = "pris";
+        r1 = 1;
+        r2 = -1;
 
-        success(results);
-      });
+    }
+      nyArr =  arr.sort(sorteringfunk);
+      success(nyArr);
+
+    function sorteringfunk(a, b) {//brukes for å sortere array med array
+     if (a[index] < b[index]){
+       return r1;
+     }else if (a[index] > b[index]){
+       return r2;
+     }
+     return 0;
+   }
+  }
+
+  visKlasse(klasse, arrInn, success){
+    console.log(klasse);
+    if(klasse == "Sykkeltype"){
+      success(arrInn);
+      return;
+    }
+    let arr = arrInn.slice();//lager en klone av arrayen for ikke å endre den originale
+    for(let i = arr.length-1; i >= 0; i--){
+      if(arr[i].klassenavn !== klasse){
+        arr.splice(i,1);
+      }
+    }
+    success(arr);
   }
 }
 
