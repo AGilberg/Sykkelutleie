c35287a40f3422cda4d83f49ba3b159624269bc0
Annika <annikalu@stud.ntnu.no>
2019-03-28 10:19:41 +0100

    FERDIG med produktSykkel.js, endring av boxshadow og +- for antall produkter

--- src/widgets.js
+++ src/widgets.js
@@ -160,7 +160,7 @@ class ButtonInfo extends Component {
         style={{
           backgroundColor: '#f8f9fa',
           border: '2px solid lightgrey',
-          boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
+          boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23) '
         }}
         onClick={this.props.onClick}
       >
