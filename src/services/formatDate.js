21524cfe1b7f8fa44f2e97100a2e0454f08dd8f4
AGilberg <Adrian.B.Gilberg@Gmail.com>
2019-04-03 13:19:02 +0200

    Endret på visningen av aktive bestillinger. Lagt inn funksjon for datoformatering i egen fil (må endre slik at andre filer også bruker denne funksjonen).

---
+++ src/services/formatDate.js
@@ -0,0 +1,13 @@
+export default function formatDate(date) {
+  let day = date.getDate();
+  if (day < 10) {
+    day = '0' + day.toString();
+  }
+  let month = date.getMonth() + 1;
+  if (month < 10) {
+    month = '0' + month.toString();
+  }
+  let year = date.getFullYear();
+
+  return day + '/' + month + '/' + year;
+}
