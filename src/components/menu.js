4c664370fadd969ed724b3fce67e05503cd5ce70
Annika <annikalu@stud.ntnu.no>
2019-04-09 15:03:03 +0200

    Styling

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,7 +4,6 @@ import { Card, List, Row, Column, NavBar, Button, Form } from '../widgets';
 import { history } from '../index.js';
 import { loginService } from '../services/LoginService';
 
-
 class Menu extends Component {
   // mounted(){// FIXME: legg til for å bruke innloggingen og utlogging, IKKE FJERN
   //   if(!cartService.getIsLoggedInn()){
@@ -29,25 +28,24 @@ class Menu extends Component {
           />
         >
           <NavBar.Link to="/aktivebestillinger">Alle bestillinger</NavBar.Link>
-          <NavBar.Link to="/ansatt">Ansatt</NavBar.Link>
 
-           <NavBar.Link to="/sykkelID">Sykkel ID</NavBar.Link>
-           <div className="js-hidden" id="leieperiode" />
+          <NavBar.Link to="/sykkelID">Sykkel ID</NavBar.Link>
+          <div className="js-hidden" id="leieperiode" />
 
-           <div
-             style={{
-               color: 'rgba(0, 0, 0, 0.5)',
-               fontSize: '18px',
-               marginTop: '5px',
-               position: 'absolute',
-               right: '10px',
-               padding: '8px',
-               paddingBottom: '12px',
-               fontWeight: 'bold'
-             }}
-           >
-             <Button.Info onClick={loginService.logout}>Logg ut</Button.Info>
-           </div>
+          <div
+            style={{
+              color: 'rgba(0, 0, 0, 0.5)',
+              fontSize: '18px',
+              marginTop: '5px',
+              position: 'absolute',
+              right: '10px',
+              padding: '8px',
+              paddingBottom: '12px',
+              fontWeight: 'bold'
+            }}
+          >
+            <Button.Info onClick={loginService.logout}>Logg ut</Button.Info>
+          </div>
         </NavBar>
       </div>
     );
