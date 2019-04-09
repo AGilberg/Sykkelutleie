1ca0dd0ab7a7c032c3eb61ccdd910050f37b0890
AGilberg <Adrian.B.Gilberg@Gmail.com>
2019-04-09 12:42:55 +0200

    Endret på søkemekanismen, slik at den benytter seg mer av fordelene med React og blir mer effektiv.

--- src/components/kundesok.js
+++ src/components/kundesok.js
@@ -12,11 +12,20 @@ import varsel from '../services/notifications.js';
 class Kundesøk extends Component {
   kunder = null;
 
+  state = {sok: "", kategori: "navn"};
+
+
+  onInputChange = (event, data) => this.setState({sok: event.target.value.toLowerCase()});
+  onSelectChange = (event) => this.setState({kategori: event.target.value});
+
   render() {
     if (!this.kunder)
       return (
         <ReactLoading className="spinner fade-in" type="spinningBubbles" color="lightgrey" height="20%" width="20%" />
       );
+      console.log(this.state);
+      const kunder = this.kunder.filter(kunde => kunde[this.state.kategori].toString().toLowerCase().includes(this.state.sok));
+      console.log(kunder);
     return (
       <div>
         {/* Søk etter registrerte kunder */}
@@ -34,24 +43,25 @@ class Kundesøk extends Component {
             style={{ border: '2px solid lightgrey', borderRadius: '4px', padding: '5px' }}
             id="sok"
             placeholder="Søk i kundedatabasen"
-            onChange={this.sok}
+            onChange={this.onInputChange}
           />
           <select
             id="kategori"
             className="shadow"
             style={{ border: '2px solid lightgrey', borderRadius: '4px', padding: '5px' }}
+            onChange={this.onSelectChange}
           >
-            <option value="0">Navn</option>
-            <option value="1">Epost</option>
-            <option value="2">Telefon</option>
-            <option value="3">Adresse</option>
-            <option value="4">Fødselsdato</option>
+            <option value="navn">Navn</option>
+            <option value="mail">Epost</option>
+            <option value="tlf">Telefon</option>
+            <option value="adresse">Adresse</option>
+            <option value="fodt">Fødselsdato</option>
           </select>
         </div>
 
         <div id="kunderesultat" style={{ padding: '12px', margin: '5px' }}>
           <div>
-            {this.kunder.map(kunde => (
+            {kunder.map(kunde => (
               <Card className="brBottom shadow" key={kunde.person_id}>
                 <ul style={{ paddingTop: '15px' }}>
                   <li>Navn: {kunde.fornavn + ' ' + kunde.etternavn}</li>
@@ -99,30 +109,13 @@ class Kundesøk extends Component {
   }
   mounted() {
     kundeService.getKunder(kunder => {
-      this.kunder = kunder;
+      this.kunder = kunder.map(kunde => ({
+        ...kunde,
+        navn: `${kunde.fornavn} ${kunde.etternavn}`
+      }));
     });
   }
 
-// Søkefunksjon for å søke etter registrerte kunder
-  sok() {
-    var input, filter, type, div, ul, i, li, txt;
-    input = document.getElementById('sok');
-    filter = input.value.toUpperCase();
-    type = document.getElementById('kategori').value; // Henter typekategori for søk
-    div = document.getElementById('kunderesultat').children[0];
-    ul = div.getElementsByTagName('ul');
-    for (i = 0; i < this.kunder.length; i++) {
-      li = ul[i].getElementsByTagName('li');
-      txt = li[type].textContent || li[type].innerText;
-      if (txt.toUpperCase().indexOf(filter) > -1) { // Fjerner case-sensitivitet
-        ul[i].style.display = '';
-        ul[i].closest('div.card').style.display = '';
-      } else {
-        ul[i].style.display = 'none';
-        ul[i].closest('div.card').style.display = 'none'; // Fjerner kunder som ikke matcher
-      }
-    }
-  }
 
   slettKunde(kunde){
     console.log(kunde);
